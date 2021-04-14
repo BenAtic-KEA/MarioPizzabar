568c3653be8be8d9a59395b6a4da1595a8e9d117
Mark Hansen <Mark024e@stud.kea.dk>
2021-04-14 15:53:40 +0200

    Tilføjet beskrivelser af koden til PizzaMenu.java

--- MarioPizzabarSystem/src/PizzaMenu.java
+++ MarioPizzabarSystem/src/PizzaMenu.java
@@ -13,28 +13,33 @@ public class PizzaMenu {
     public static void listOfPizzas()
             throws FileNotFoundException {
 
-
+        // lokalisere og opretter scanner af Pizza.csv filen.
         File pizzaFile = new File("MarioPizzabarSystem/Pizzaer/Pizza.csv");
         Scanner pizzaFileReader = new Scanner(pizzaFile);
 
+        // springer metadata over i Pizza.csv.
         pizzaFileReader.nextLine();
 
         while (pizzaFileReader.hasNextLine()) {
+            // gemmer hver linje i currentLine, så den kan splittes til et array (currentPizza)
             String currentLine = pizzaFileReader.nextLine();
             String[] currentPizza = currentLine.split(";");
 
+            // definere Pizzaens attributes
             int nr = Integer.parseInt(currentPizza[0].trim());
             String name = currentPizza[1].trim();
             String ingredients = currentPizza[2].trim();
             int price = Integer.parseInt(currentPizza[3].trim());
 
+            //bruger pizza constructeren til at lave et object.
             Pizza tempPizza = new Pizza(nr, name, ingredients, price);
 
+            // gemmer objectet i min ArrayList pizzaMenu og fortsætter mit while Loop
             pizzaMenu.add(tempPizza);
         }
 
     }
-
+        // getPizza bruges til at hente 1 specifikt objekt i pizzaMenu.
         public static Pizza getPizza(int pizzaNr){
             return pizzaMenu.get(pizzaNr);
 
