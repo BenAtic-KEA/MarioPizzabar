9b46c4f6eb7bf19519671a6446175e5bb59df975
mads77m7@s <mads77m7@stud.kea.dk>
2021-04-14 14:30:02 +0200

    Created pizza class

---
+++ MarioPizzabarSystem/src/Pizza.java
@@ -0,0 +1,49 @@
+
+public class Pizza {
+        private int nr;
+        private String name;
+        private String ingredients;
+        private int price;
+
+
+        public Pizza(int nr, String name, String ingredients, int price) {
+            this.nr = nr;
+            this.name = name;
+            this.ingredients = ingredients;
+            this.price = price;
+        }
+
+        public int getNr() {
+            return this.nr;
+        }
+
+        public void setNr(int newNr) {
+            this.nr = newNr;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String newName){
+            this.name = newName;
+        }
+
+        public String getIngredients() {
+            return this.ingredients;
+        }
+
+        public void setIngredients(String newIngredients){
+            this.ingredients = newIngredients;
+        }
+
+        public int getPrice() {
+            return this.price;
+        }
+
+        public void setPrice(int newPrice) {
+            this.price = newPrice;
+
+        }
+    }
+
