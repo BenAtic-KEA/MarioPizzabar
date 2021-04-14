520f805133ca72ed81ff993531d96f1d700b49ba
mads77m7@s <mads77m7@stud.kea.dk>
2021-04-14 15:20:23 +0200

    Created pizza class commented

--- MarioPizzabarSystem/src/Pizza.java
+++ MarioPizzabarSystem/src/Pizza.java
@@ -5,14 +5,14 @@ public class Pizza {
         private String ingredients;
         private int price;
 
-
+        //constructor
         public Pizza(int nr, String name, String ingredients, int price) {
             this.nr = nr;
             this.name = name;
             this.ingredients = ingredients;
             this.price = price;
         }
-
+        // getters and setters
         public int getNr() {
             return this.nr;
         }
@@ -45,7 +45,7 @@ public class Pizza {
             this.price = newPrice;
 
         }
-
+        //Overiding tostring
         @Override
         public String toString(){
             return nr + "" + name + "" + ingredients + "" + price;
