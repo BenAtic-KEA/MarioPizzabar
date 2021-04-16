17976dd798c7caf146178cddeab38bea7c577ba2
mads77m7@s <mads77m7@stud.kea.dk>
2021-04-16 11:26:58 +0200

    Created pizza class commented added spaces added method for new orderline in menu and editorderline in orderlist added display order added remove orderline method

--- MarioPizzabarSystem/src/Order.java
+++ MarioPizzabarSystem/src/Order.java
@@ -66,7 +66,7 @@ public class Order {
 
         }
 
-
+    //metode til at fjerne en ordrelinje fra menuen
     public void removeOrderLine() {
         Scanner sc = new Scanner(System.in);
         System.out.println("fjern ordrelinje? y/n");
